0c130ee7e83a281a5321610346f92c9dba706da0
Jonathan Fieldsend <J.E.Fieldsend@exeter.ac.uk>
2015-09-24 14:45:54 +0100

    reduced CorpusGp size

--- corpus/CorpusGP.java
+++ corpus/CorpusGP.java
@@ -47,57 +47,15 @@ public class CorpusGP
                 }
             }
         }
-                
-        // Find all tests currently in corpus
-        /*for (Tuple2<Path, String> t : sources){
-            String temp = t.getFirst().toString();
-            if (temp.endsWith("_tests.ss")){
-                Program p = new SchemeProgram();
-                p.setProgram(t.getSecond()); 
-                System.out.println(js.load(p.getProgramAsString()));
-                System.out.println(js.getGlobalValue("testcases"));
-                System.out.println(js.eval("(length testcases)"));
-                
-                System.out.println(js.eval("(list-ref testcases 1)"));
-                System.out.println(js.eval("(list-ref (list-ref testcases 1) 0)"));
-                inputs.add(js.eval("(list-ref (list-ref testcases 1) 0)"));
-                outputs.add(js.eval("(list-ref (list-ref testcases 1) 1)"));
-                testList.add(temp.substring(0,temp.length()-9) + ".ss");
-            }
-        }*/
-        /*
-        int index = 0;
-        // add to corpus all programs for which there are current tests
-        for (Tuple2<Path, String> t : sources){       
-            if (testList.contains(t.getFirst().toString())){
-                System.out.println("Adding: " + t.getFirst().toString());
-                Program p = new SchemeProgram();
-                p.setProgram(t.getSecond()); 
-                corpus.addToCorpus(p);
-                programs.add(p);
-                System.out.println("Added: " + t.getFirst().toString());
-                // now evaluate on test data
-                System.out.println("evaluating");
-                System.out.println(js.load(p.getProgramAsString()));
-                
-                try {
-                    System.out.println(js.eval("(prog (list-ref (list-ref testcases 1) 0) )");
-                } catch(Exception e) {
-                    System.out.println(e.getMessage());
-                }
-                index++;
-            }
-        }*/
+         
         for (Tuple2<Program, Program> p : programAndTests) {
             corpus.addToCorpus(p.getFirst());
             // define program and test cases in the Scheme environment
             js.load(p.getFirst().getProgramAsString());
             js.load(p.getSecond().getProgramAsString());
             System.out.println("evaluating");
-            System.out.println(js.getGlobalValue("prog"));
-            System.out.println(js.getGlobalValue("testcases"));
             // evalate program on its zeroth testcase input
-            System.out.println(js.eval("(prog (list-ref (list-ref testcases 0) 0) )"));
+            js.eval("(prog (list-ref (list-ref testcases 0) 0) )");
         }
         
         
